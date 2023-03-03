753ccf66cbf51b7945a9901e5d5c52e9a207e2d1
Andrew Gamal <58505236+Andrew-Gamal@users.noreply.github.com>
2023-03-03 13:34:46 +0200

    Add male character with all animations + fix cloud

---
+++ Assets/_APP/Scripts/Manager/ProfileSelection.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProfileSelection : MonoBehaviour
+{
+    public ProfileSelectionSO profileSelection;
+
+    private void Start()
+    {
+        if (profileSelection.profileType == profileType.male)
+        {
+
+        }
+        else if (profileSelection.profileType == profileType.female)
+        {
+
+        }
+    }
+}
